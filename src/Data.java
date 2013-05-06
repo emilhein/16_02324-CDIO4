8e728c6f41da6e91eddc20db55da6ca38662085a
Jens Werner Nielsen <jenswernernielsen@gmail.com>
2013-05-06 15:05:38 +0200

    Tilføjet Data lag

---
+++ src/Data.java
@@ -0,0 +1,13 @@
+﻿public class Data {
+
+	private ArrayList<Operator> operators = new ArrayList<Operator>();
+	
+	public Data() {
+		//# TODO: Tilføj falske operatører her...
+	}
+	
+	public Operator[] getOperators() {
+		return operators.toArray(new Operator[operators.size()]);
+	}
+
+}
