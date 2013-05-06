d238dbbf0396e47a2c3bcb0d31b0dff762b89f62
Jens Werner Nielsen <jenswernernielsen@gmail.com>
2013-05-06 15:17:21 +0200

    Lavet lidt i Data

--- src/Data.java
+++ src/Data.java
@@ -3,7 +3,9 @@
 	private ArrayList<Operator> operators = new ArrayList<Operator>();
 	
 	public Data() {
-		//# TODO: Tilføj falske operatører her...
+		operators.add(new Operator(1, "Test 1", "aa", "1111111111", "1111111"));
+		operators.add(new Operator(2, "Test 2", "bb", "2222222222", "2222222"));
+		operators.add(new Operator(3, "Test 3", "cc", "3333333333", "3333333"));
 	}
 	
 	public Operator[] getOperators() {
